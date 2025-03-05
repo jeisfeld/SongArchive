3132f84943af971b84ba472a72249331667b8386
Jörg Eisfeld <github@jeisfeld.de>
2025-03-05 12:40:59 +0100

    Initial commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,65 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.ksp)
+}
+
+android {
+    namespace = "de.jeisfeld.songarchive"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "de.jeisfeld.songarchive"
+        minSdk = 26
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+    buildFeatures {
+        compose = true
+    }
+}
+
+dependencies {
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.activity.compose)
+
+    // Jetpack Compose UI
+    implementation(libs.ui)
+    implementation(libs.androidx.material)
+
+    // Room für SQLite-Datenbank
+    implementation(libs.androidx.room.runtime)
+    implementation(libs.androidx.room.ktx)
+    implementation(libs.androidx.material3.android)
+    implementation(libs.androidx.ui.tooling.preview.android)
+    ksp(libs.androidx.room.compiler)
+
+    // Retrofit für den HTTP-Download
+    implementation(libs.retrofit)
+    implementation(libs.converter.gson)
+
+    // Coil für das Laden von Bildern
+    implementation(libs.coil.compose)
+
+    // WorkManager für Hintergrund-Download
+    implementation(libs.androidx.work.runtime.ktx)
+}
