d10df6f7b5937b97a911d13a5bd4b6b3f2beebd6
Jörg Eisfeld <github@jeisfeld.de>
2025-03-27 13:32:03 +0100

    Initial preparation of version 1.1.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "de.jeisfeld.songarchive"
         minSdk = 26
         targetSdk = 35
-        versionCode = 9
-        versionName = "1.1.3"
+        versionCode = 10
+        versionName = "1.1.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
