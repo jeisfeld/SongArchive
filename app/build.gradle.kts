6dbfb5f03aa6dd5d7453f020e1a0dc03cc351d32
Jörg Eisfeld <github@jeisfeld.de>
2025-10-10 02:03:59 +0200

    Initial preparation of version 1.2.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,8 +32,8 @@ android {
         applicationId = "de.jeisfeld.songarchive"
         minSdk = 26
         targetSdk = 35
-        versionCode = 20
-        versionName = "1.2.4"
+        versionCode = 21
+        versionName = "1.2.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         val escapedApiKey = firebaseCloudVisionApiKey
