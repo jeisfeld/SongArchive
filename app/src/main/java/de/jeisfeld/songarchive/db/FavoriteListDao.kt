380176a9468d08c5820ac00f7370b7af94ad5008
Jörg Eisfeld <github@jeisfeld.de>
2025-12-15 22:34:53 +0100

    Update KSP version and Room import

--- app/src/main/java/de/jeisfeld/songarchive/db/FavoriteListDao.kt
+++ app/src/main/java/de/jeisfeld/songarchive/db/FavoriteListDao.kt
@@ -5,6 +5,7 @@ import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import androidx.room.Transaction
 import androidx.room.Update
 import de.jeisfeld.songarchive.db.FavoriteListSong
 import de.jeisfeld.songarchive.db.Song
